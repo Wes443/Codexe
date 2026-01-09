2059aef16fc2d4a9ba2d191ddde170540dc8b1c7
Welsey Wong <wesley.wong04@gmail.com>
2026-01-09 17:19:35 -0600

    add create user junit test

--- backend/src/test/java/com/example/codexe/CodexeApplicationTests.java
+++ backend/src/test/java/com/example/codexe/CodexeApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.codexe;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.codexe.model.User;
+import com.example.codexe.service.UserService;
 
 @SpringBootTest
 class CodexeApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
-	void contextLoads() {
+	void testCreateUser() {
+		User user = new User(
+			"example2@email.com",
+			"root",
+			"password",
+			0,
+			0,
+			0
+		);
+
+		user = userService.createUser(user);
+		assertNotNull(user);
 	}
 
 }
