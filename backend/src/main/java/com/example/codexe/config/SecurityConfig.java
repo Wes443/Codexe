cf6a23e09ab79915dc6b1ed063a009db7aaca28a
Welsey Wong <wesley.wong04@gmail.com>
2026-01-09 12:48:43 -0600

    Implement temp security config

---
+++ backend/src/main/java/com/example/codexe/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.codexe.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // Enable CORS
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            // Disable CSRF for JWT-based authentication (to be configured)
+            .csrf(csrf -> csrf.disable())
+            //authorization configuration
+            .authorizeHttpRequests(auth -> auth
+                // for development purposes, permit all requests
+                .anyRequest().permitAll()
+                
+                // .requestMatchers("/public/**").permitAll()
+                // .anyRequest().authenticated()
+            )
+            // Use HTTP Basic Authentication
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+        
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        //only allow requests from localhost:3000 (frontend)
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        //allowed response methods
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        //allow all headers
+        config.setAllowedHeaders(List.of("*"));
+        //allow credentials
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //apply this configuration to all endpoints
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
