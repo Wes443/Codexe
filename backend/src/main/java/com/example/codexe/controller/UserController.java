f268661ef9422ceafed743e4c715dcee940c0b23
Welsey Wong <wesley.wong04@gmail.com>
2026-01-15 12:17:47 -0600

    formatting changes

--- backend/src/main/java/com/example/codexe/controller/UserController.java
+++ backend/src/main/java/com/example/codexe/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -42,7 +41,6 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody LoginRequest credentials){
         User user = userService.validateCredentials(credentials.getUsername(), credentials.getPassword());
-
         return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
