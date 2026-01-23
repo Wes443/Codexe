c11098a69843f8339799dbfa35c5b1a430d9c21d
Welsey Wong <wesley.wong04@gmail.com>
2026-01-22 18:42:01 -0600

    update frontend request

--- frontend/src/services/userServices.js
+++ frontend/src/services/userServices.js
@@ -2,11 +2,11 @@ import api from './api'
 
 //user services
 export async function createUser(userData){
-    const response = await api.post("/create-user", userData);
+    const response = await api.post("/auth/create-user", userData);
     return response.data;
 }
 
 export async function loginUser(credentials){ 
-    const response = await api.post("/login", credentials);
+    const response = await api.post("/auth/login", credentials);
     return response.data;
 }
