61aab24424ef89cf817019435569418aca6fdb1b
Welsey Wong <wesley.wong04@gmail.com>
2026-08-14 13:23:45 -0500

    add home button and profile nav

--- src/icons.jsx
+++ src/icons.jsx
@@ -1,5 +1,15 @@
 import LogoSvg from "./assets/logo.svg?react";
+import UserSvg from "./assets/user-icon.svg?react";
+import LogoutSvg from "./assets/logout-icon.svg?react";
 
 export function LogoIcon(props) {
     return <LogoSvg {...props} />;
+}
+
+export function UserIcon(props) {
+    return <UserSvg {...props} />;
+}
+
+export function LogoutIcon(props) {
+    return <LogoutSvg {...props} />;
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,21 +3,52 @@ import { useAuth } from "../auth/AuthContext";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import { useNavigate } from "react-router-dom";
+import HomeButton from "../components/HomeButton";
+import { useEffect, useState } from "react";
+import { UserIcon, LogoutIcon } from "../icons";
 
 function Dashboard() {
-    const { user, userDoc } = useAuth();
-
+    const { user, userDoc, loading } = useAuth();
+    const [ isGuest, setIsGuest ] = useState(true);
     const navigate = useNavigate();
-    
+
+    //check if the user is signed in or a guest
+    useEffect(() => {
+        if(loading) return;
+        if(user && userDoc) setIsGuest(false);
+
+    }, [user, userDoc, loading]);
+
+    //function to handle signout 
 	const handleSignOut = async () => {
 		await signOut(auth);
 		navigate("/login");
 	};
 
+    //function to handle when the user icon is pressed
+    function handleUserIcon() {
+        if(isGuest){
+            navigate("/login");
+        }else{
+            navigate("/profile");
+        }
+    }
+
     return (
-        <div>
-            {userDoc && <p>{userDoc.firstName}</p>}
-            {user && <button onClick={handleSignOut}>Sign Out</button>}
+        <div className={styles["window"]}>
+            <HomeButton />
+            
+            <div className={styles["account-nav-container"]}>
+                <div className={styles["user-icon"]} onClick={handleUserIcon}>
+                    <UserIcon style={{width: "32", height: "32"}}/>
+                </div>
+                
+                {!isGuest && 
+                    <div className={styles["logout-icon"]} onClick={handleSignOut}>
+                        <LogoutIcon style={{width: "32", height: "32"}} />
+                    </div>
+                }
+            </div>
         </div>
     );
 }
