49c2aa5cb1b8e5083dee91b3e1f5f80c2b2b6b80
Welsey Wong <wesley.wong04@gmail.com>
2026-08-17 13:17:09 -0500

    css styling to session results page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -4,11 +4,12 @@ import { useAuth } from "../auth/AuthContext";
 import { useEffect, useState} from "react";
 import HomeButton from "../components/HomeButton";
 import AccountBar from "../components/AccountBar";
+import { NextIcon } from "../icons";
 
 function Result() {
     const { user, userDoc, loading } = useAuth();
     const [ isGuest, setIsGuest ] = useState(true);
-
+    const [ returnHover, setReturnHover ] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -43,14 +44,46 @@ function Result() {
         <div className={styles["session-container"]}>
             <HomeButton />
             <AccountBar isGuest={isGuest} />
-            <p>{accuracy}</p>
-            <p>{stdWpm}</p>
-            <p>{rawWpm}</p>
-            <p>{language}</p>
-            <p>{lines}</p>
-            <p>{correct}</p>
-            <p>{incorrect}</p>
-            <p>{time}</p>
+            <div className={styles["results"]}>
+                <div className={styles["wpm"]}>
+                    <p className={styles["header-text"]}>wpm</p>
+                    <p className={styles["header-text"]} style={{color: "var(--accent-text)"}}>{stdWpm}</p>
+                    <p className={styles["accent-text"]}>raw</p>
+                    <p className={styles["accent-text"]}>{rawWpm}</p>
+                </div>
+
+                <div className={styles["acc"]}>
+                    <p className={styles["header-text"]}>acc</p>
+                    <p className={styles["header-text"]} style={{color: "var(--accent-text)"}}>{accuracy.toFixed(0)}%</p>
+                    <div className={styles["acc-chars"]}>
+                        <p style={{color: "var(--accent-text)", fontSize: "16px"}}>total/correct/incorrect</p>
+                        <p className={styles["accent-text"]}>{correct + incorrect}/{correct}/{incorrect}</p>
+                    </div>
+                </div>
+                
+                <div className={styles["time"]}>
+                    <p className={styles["header-text"]}>time</p>
+                    <p className={styles["header-text"]} style={{color: "var(--accent-text)"}}>{time}s</p>
+                </div>
+
+                <div className={styles["mode"]}>
+                    <p className={styles["header-text"]}>mode</p>
+                    <p className={styles["accent-text"]}>~{lines}</p>
+                    <p className={styles["accent-text"]}>lines</p>
+                    <p className={styles["accent-text"]}>{language}</p>
+                </div>
+            </div>
+            <div className={styles["return"]}>
+                <span
+                    onMouseEnter={() => setReturnHover(true)}
+                    onMouseLeave={() => setReturnHover(false)}
+                    onClick={() => navigate("/")}
+                    style={{cursor: "pointer"}}
+                >
+                    <NextIcon style={{transform: "scaleX(-1)", color: returnHover ? "var(--hover-text)" : "var(--default-text)" }}/>
+                </span>
+                <p style={{color: "var(--default-text)", opacity: returnHover ? "100%": "0%"}}>return</p>
+            </div>
         </div>
     );
 }
